ef2b9ace06fe3b77c155a0aeb38078a0d18918b4
Van Nguyen <van.nguyenthi@asnet.com.vn>
2020-12-22 09:40:13 +0700

    Comment option expires

--- utils/cookies.ts
+++ utils/cookies.ts
@@ -7,10 +7,10 @@ const cookie = (res, name, value, options = {}) => {
   const stringValue =
     typeof value === 'object' ? 'j:' + JSON.stringify(value) : String(value)
 
-  if ('maxAge' in options) {
-    options.expires = new Date(Date.now() + options.maxAge)
-    options.maxAge /= 1000
-  }
+  // if ('maxAge' in options) {
+  //   options.expires = new Date(Date.now() + options.maxAge)
+  //   options.maxAge /= 1000
+  // }
 
   res.setHeader('Set-Cookie', serialize(name, String(stringValue), options))
 }
