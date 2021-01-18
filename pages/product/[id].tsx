9d75e26963c9b57d43fa304e24d881f57f333003
Van Nguyen <van.nguyenthi@asnet.com.vn>
2021-01-18 23:54:40 +0700

    Check router

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -20,13 +20,27 @@ const fetcher = async (url) => {
 
 export default function Person({allProducts}) {
   const { query } = useRouter()
-  // const { data, error } = useSWR(
-  //   () => query.id && `/api/people/${query.id}`,
-  //   fetcher
-  // )
   console.log('allProducts555555555555555555555555555', allProducts)
   //if (error) return <div>{error.message}</div>
   //if (!data) return <div>Loading...</div>
+  const dataProduct = allProducts && allProducts.filter(({ node }) => node._meta.uid === query.id)
+  const dataImagesProduct = dataProduct && dataProduct.map(({ node }) => node.images) 
+  const dataProductLength = dataProduct.length > 0
+  const productTypePopular = dataProductLength && allProducts.filter(({ node }) => node.type_popular && !node.blog_popular)
+  const sexType = dataProductLength && dataProduct[0].node.sex
+  const imagesRelative = []
+  const imagesListProduct = Object.values(dataImagesProduct[0])
+  Object.keys(imagesListProduct).forEach(function (item) {
+    if(typeof imagesListProduct[item]== 'object' && imagesListProduct[item]!= null){
+      if(imagesListProduct[item].url) {
+        imagesRelative.push(imagesListProduct[item])
+      }
+    }
+  })
+  
+  const productReviewComments = dataProduct[0].node.body && dataProduct[0].node.body.filter(({ type }) => type === 'comments')
+  const numComments = productReviewComments && productReviewComments.length
+
 
   return (
     <>
