350161f929d11b88aaa11b1771b3452468d6fb7a
Van Nguyen <van.nguyenthi@asnet.com.vn>
2021-01-18 17:12:09 +0700

    Check router

--- next.config.js
+++ next.config.js
@@ -14,5 +14,3 @@ module.exports = withImages(
     "pagesDir": './src/pages'
   },
 )
-
-
--- pages/admin/person/[id].tsx
+++ pages/admin/person/[id].tsx
@@ -58,14 +58,14 @@ export default function Person() {
              {data.name}
             </p>
             <p className="text-gray-500 font-medium">
-              Product Engineer
+              Product EngineerIDDDDDDDDDDDDDDDDDDDDDDDDD
             </p>
           </div>
           <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
         </div>
       </div>
       <Link href="/admin/person">
-        <a className='link'>Back to person list</a>
+        <a className='link mt-8'>Back to person list</a>
       </Link>
     </>
   )
