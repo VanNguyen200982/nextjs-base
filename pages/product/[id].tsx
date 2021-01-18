75a206a8424b8ffb6ebe499b3043dd38bc9a2382
Van Nguyen <van.nguyenthi@asnet.com.vn>
2021-01-18 23:58:18 +0700

    Check router

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,11 +1,17 @@
 import { useRouter } from 'next/router'
-import useSWR from 'swr'
-import Link from 'next/link'
+import Head from 'next/head'
 
 
 import { getAllDatasForProducts } from '../../utils/api'
 import { navListProduct } from '../../utils/constants'
 
+// Components
+import Layout from '../../components/layout'
+import ProductList from '../../components/products'
+import Banner from '../../components/banner'
+import Tabs from '../../components/tab'
+import SliderNav from '../../components/product/slide-nav'
+import InfoProduct from '../../components/product/info'
 
 const fetcher = async (url) => {
   const res = await fetch(url)
@@ -44,37 +50,28 @@ export default function Person({allProducts}) {
 
   return (
     <>
-      <table className="table-auto border-separate border border-green-800">
-        <thead>
-          <tr>
-            <th className="border border-green-600">Name</th>
-            <th className="border border-green-600">Height</th>
-            <th className="border border-green-600">Mass</th>
-            <th className="border border-green-600">Hair color</th>
-            <th className="border border-green-600">Skin color</th>
-            <th className="border border-green-600">Eye color</th>
-            <th className="border border-green-600">Gender</th>
-          </tr>
-        </thead>
-       
-      </table>
-      <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-        
-        <div className="text-center space-y-2 sm:text-left">
-          <div className="space-y-0.5">
-            {/* <p className="text-lg text-black font-semibold">
-             {data.name}
-            </p> */}
-            <p className="text-gray-500 font-medium">
-              Product EngineerIDDDDDDDDDDDDDDDDDDDDDDDDD
-            </p>
+      <Layout preview={'product'}>
+        <Head>
+          <title> Product Detail</title>
+        </Head>
+        {dataProductLength && 
+          <div className=''>
+            <Banner sex={sexType} navList={navListProduct}/>
+            <div className="flex flex-col m-auto -mt-20 items-center justify-between bg-white w-11/12 h-full border-solid border	sborder-gray-50">
+            <div className="flex w-full">
+              <InfoProduct product={dataProduct[0].node}/>
+              {imagesRelative.length > 0 &&
+                <SliderNav dataProduct={imagesRelative}/>
+              }
+            </div>
+              {numComments > 0 &&
+                <Tabs reviews={productReviewComments}/>
+              }
+              <ProductList allDatas={productTypePopular} loadMoreNumber={3}/>
+            </div>
           </div>
-          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
-        </div>
-      </div>
-      <Link href="/admin/person">
-        <a className='link mt-8'>Back to person list</a>
-      </Link>
+        }
+      </Layout>
     </>
   )
 }
