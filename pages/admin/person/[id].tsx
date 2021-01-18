c5737ab6da1e6c0c838f302ed64216acbb533147
Van Nguyen <van.nguyenthi@asnet.com.vn>
2021-01-18 17:06:24 +0700

    Check router

--- pages/admin/person/[id].tsx
+++ pages/admin/person/[id].tsx
@@ -1,68 +1,72 @@
 import { useRouter } from 'next/router'
-import Head from 'next/head'
+import useSWR from 'swr'
+import Link from 'next/link'
 
-import { getAllDatasForProducts } from '../../../utils/api'
-import { navListProduct } from '../../../utils/constants'
 
-// Components
-import Layout from '../../../components/layout'
-import ProductList from '../../../components/products'
-import Banner from '../../../components/banner'
-import Tabs from '../../../components/tab'
-import SliderNav from '../../../components/product/slide-nav'
-import InfoProduct from '../../../components/product/info'
 
-export default function Product({ preview, allProducts }){
-  const { query } = useRouter()
-  const dataProduct = allProducts && allProducts.filter(({ node }) => node._meta.uid === query.id)
-  const dataImagesProduct = dataProduct && dataProduct.map(({ node }) => node.images) 
-  const dataProductLength = dataProduct.length > 0
-  const productTypePopular = dataProductLength && allProducts.filter(({ node }) => node.type_popular && !node.blog_popular)
-  const sexType = dataProductLength && dataProduct[0].node.sex
-  const imagesRelative = []
-  const imagesListProduct = Object.values(dataImagesProduct[0])
-  Object.keys(imagesListProduct).forEach(function (item) {
-    if(typeof imagesListProduct[item]== 'object' && imagesListProduct[item]!= null){
-      if(imagesListProduct[item].url) {
-        imagesRelative.push(imagesListProduct[item])
-      }
-    }
-  })
-  
-  const productReviewComments = dataProduct[0].node.body && dataProduct[0].node.body.filter(({ type }) => type === 'comments')
-  const numComments = productReviewComments && productReviewComments.length
+const fetcher = async (url) => {
+  const res = await fetch(url)
+  const data = await res.json()
 
-   return (
-    <>
-      <Layout preview={'product'}>
-        <Head>
-          <title> Product Detail</title>
-        </Head>
-        {dataProductLength && 
-          <div className=''>
-            <Banner sex={sexType} navList={navListProduct}/>
-            <div className="flex flex-col m-auto -mt-20 items-center justify-between bg-white w-11/12 h-full border-solid border	sborder-gray-50">
-            <div className="flex w-full">
-              <InfoProduct product={dataProduct[0].node}/>
-              {imagesRelative.length > 0 &&
-                <SliderNav dataProduct={imagesRelative}/>
-              }
-            </div>
-              {numComments > 0 &&
-                <Tabs reviews={productReviewComments}/>
-              }
-              <ProductList allDatas={productTypePopular} loadMoreNumber={3}/>
-            </div>
-          </div>
-        }
-      </Layout>
-    </>
-  )
+  if (res.status !== 200) {
+    throw new Error(data.message)
+  }
+  return data
 }
 
-export async function getServerSideProps({previewDataProduct }) {
-  const allProducts = await getAllDatasForProducts(previewDataProduct)
-  return {
-    props: { allProducts },
-  }
+export default function Person() {
+  const { query } = useRouter()
+  const { data, error } = useSWR(
+    () => query.id && `/api/people/${query.id}`,
+    fetcher
+  )
+
+  if (error) return <div>{error.message}</div>
+  if (!data) return <div>Loading...</div>
+
+  return (
+    <>
+      <table className="table-auto border-separate border border-green-800">
+        <thead>
+          <tr>
+            <th className="border border-green-600">Name</th>
+            <th className="border border-green-600">Height</th>
+            <th className="border border-green-600">Mass</th>
+            <th className="border border-green-600">Hair color</th>
+            <th className="border border-green-600">Skin color</th>
+            <th className="border border-green-600">Eye color</th>
+            <th className="border border-green-600">Gender</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="border border-green-600">{data.name}</td>
+            <td className="border border-green-600">{data.height}</td>
+            <td className="border border-green-600">{data.mass}</td>
+            <td className="border border-green-600">{data.hair_color}</td>
+            <td className="border border-green-600">{data.skin_color}</td>
+            <td className="border border-green-600">{data.eye_color}</td>
+            <td className="border border-green-600">{data.gender}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        
+        <div className="text-center space-y-2 sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">
+             {data.name}
+            </p>
+            <p className="text-gray-500 font-medium">
+              Product Engineer
+            </p>
+          </div>
+          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+        </div>
+      </div>
+      <Link href="/admin/person">
+        <a className='link'>Back to person list</a>
+      </Link>
+    </>
+  )
 }
--- pages/admin/person/index.tsx
+++ pages/admin/person/index.tsx
@@ -1,54 +1,21 @@
-import Head from 'next/head'
+import useSWR from 'swr'
+import Person from '../../../components/person'
 
-import { getAllDatasForHome, getAllDatasForProducts, getAllDataForPattern } from '../../../utils/api'
+const fetcher = (url) => fetch(url).then((res) => res.json())
 
-// Constant
-import { CMS_NAME } from '../../../utils/constants'
-import { FeatureList } from '../../../utils/constants'
+export default function Index() {
+  const { data, error } = useSWR('/api/people', fetcher)
+  console.log('datadata', data)
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
 
-// Components
-import Layout from '../../../components/layout'
-import Slide from '../../../components/slide'
-import Feature from '../../../components/feature'
-import ProductList from '../../../components/products'
-import ProductPattern from '../../../components/product-pattern'
-import Blog from '../../../components/blog'
-
-export default function Index({ preview, allDatas, allProducts, allPattern, props }) {
-  console.log('allProductsallProducts', allProducts)
-  const settings = {
-		dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
-  }
-  const dataBannerMain = allDatas.filter(({ node }) => !node.cover);
-  const dataBannerSub = allDatas.filter(({ node }) => node.cover);
-  const productBlogPopular = allProducts.filter(({ node }) => node.blog_popular);
   return (
     <>
-      <Layout preview={preview}>
-        <Head>
-          <title> Be.Pro with {CMS_NAME}</title>
-        </Head>
-        <div>
-          <Slide data={dataBannerMain} setting={settings}/>
-          <ProductList allDatas={allProducts} loadMoreNumber={4}/>
-          <Slide data={dataBannerSub} setting={settings}/>
-          <ProductPattern allDatas={allPattern}/>
-          <Blog  data={productBlogPopular}/>
-        </div>
-      </Layout>
+      <ul>
+        {data.map((p, i) => (
+          <Person key={i} person={p} />
+        ))}
+      </ul>
     </>
   )
-}
-
-export async function getStaticProps({ preview = false, previewDataPost, previewDataProduct, previewDataPattern }) {
-  const allDatas = await getAllDatasForHome(previewDataPost)
-  const allProducts = await getAllDatasForProducts(previewDataProduct)
-  const allPattern = await getAllDataForPattern(previewDataPattern)
-  return {
-    props: { preview, allDatas, allProducts, allPattern},
-  }
 }
