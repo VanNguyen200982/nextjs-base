aa1399f7271e7c55f12bb4caf3f62337d2ee3103
Van Nguyen <van.nguyenthi@asnet.com.vn>
2021-01-19 00:02:13 +0700

    Check router

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -54,23 +54,6 @@ export default function Person({allProducts}) {
         <Head>
           <title> Product Detail</title>
         </Head>
-        {dataProductLength && 
-          <div className=''>
-            <Banner sex={sexType} navList={navListProduct}/>
-            <div className="flex flex-col m-auto -mt-20 items-center justify-between bg-white w-11/12 h-full border-solid border	sborder-gray-50">
-            <div className="flex w-full">
-              <InfoProduct product={dataProduct[0].node}/>
-              {imagesRelative.length > 0 &&
-                <SliderNav dataProduct={imagesRelative}/>
-              }
-            </div>
-              {numComments > 0 &&
-                <Tabs reviews={productReviewComments}/>
-              }
-              <ProductList allDatas={productTypePopular} loadMoreNumber={3}/>
-            </div>
-          </div>
-        }
       </Layout>
     </>
   )
